688fe9e78e6c35ce228ba7b005ec92455ab8fd91
Steve Konves <stephen.konves@gmail.com>
2023-06-11 14:09:07 -0700

    chore: npm run create-snapshot

--- src/snapshot/v1/auth.ts
+++ src/snapshot/v1/auth.ts
@@ -12,97 +12,6 @@ export interface AuthService {
 }
 export type AuthResponse = 'authorized' | 'unauthenticated' | 'unauthorized';
 
-export function authorizeGetGizmos(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  let authenticated = false;
-  if (context.isAuthenticated('oauth2Auth')) {
-    authenticated = true;
-    if (context.hasScope('oauth2Auth', 'read:gizmos')) {
-      return 'authorized';
-    }
-  }
-  return authenticated ? 'unauthorized' : 'unauthenticated';
-}
-
-export function authorizeCreateGizmo(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  let authenticated = false;
-  if (context.isAuthenticated('oauth2Auth')) {
-    authenticated = true;
-    if (context.hasScope('oauth2Auth', 'write:gizmos')) {
-      return 'authorized';
-    }
-  }
-  return authenticated ? 'unauthorized' : 'unauthenticated';
-}
-
-export function authorizeUpdateGizmo(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  let authenticated = false;
-  if (context.isAuthenticated('oauth2Auth')) {
-    authenticated = true;
-    if (
-      context.hasScope('oauth2Auth', 'write:gizmos') &&
-      context.hasScope('oauth2Auth', 'admin:gizmos')
-    ) {
-      return 'authorized';
-    }
-  }
-  return authenticated ? 'unauthorized' : 'unauthenticated';
-}
-
-export function authorizeGetWidgets(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  if (context.isAuthenticated('apiKeyAuth')) {
-    return 'authorized';
-  }
-  return 'unauthenticated';
-}
-
-export function authorizeCreateWidget(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  if (context.isAuthenticated('apiKeyAuth')) {
-    return 'authorized';
-  }
-  return 'unauthenticated';
-}
-
-export function authorizePutWidget(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  if (context.isAuthenticated('apiKeyAuth')) {
-    return 'authorized';
-  }
-  return 'unauthenticated';
-}
-
-export function authorizeGetWidgetFoo(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  if (context.isAuthenticated('apiKeyAuth')) {
-    return 'authorized';
-  }
-  return 'unauthenticated';
-}
-
-export function authorizeDeleteWidgetFoo(context?: AuthService): AuthResponse {
-  if (!context) return 'unauthenticated';
-  if (context.isAuthenticated('apiKeyAuth')) {
-    return 'authorized';
-  }
-  return 'unauthenticated';
-}
-
-export function authorizeExhaustiveFormats(
-  _context?: AuthService,
-): AuthResponse {
-  return 'authorized';
-}
-
-export function authorizeExhaustiveParams(
-  _context?: AuthService,
-): AuthResponse {
-  return 'authorized';
-}
-
 export function authorizeAllAuthSchemes(context?: AuthService): AuthResponse {
   if (!context) return 'unauthenticated';
   let authenticated = false;
@@ -149,4 +58,95 @@ export function authorizeComboAuthSchemes(context?: AuthService): AuthResponse {
     }
   }
   return authenticated ? 'unauthorized' : 'unauthenticated';
+}
+
+export function authorizeExhaustiveFormats(
+  _context?: AuthService,
+): AuthResponse {
+  return 'authorized';
+}
+
+export function authorizeExhaustiveParams(
+  _context?: AuthService,
+): AuthResponse {
+  return 'authorized';
+}
+
+export function authorizeCreateGizmo(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  let authenticated = false;
+  if (context.isAuthenticated('oauth2Auth')) {
+    authenticated = true;
+    if (context.hasScope('oauth2Auth', 'write:gizmos')) {
+      return 'authorized';
+    }
+  }
+  return authenticated ? 'unauthorized' : 'unauthenticated';
+}
+
+export function authorizeGetGizmos(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  let authenticated = false;
+  if (context.isAuthenticated('oauth2Auth')) {
+    authenticated = true;
+    if (context.hasScope('oauth2Auth', 'read:gizmos')) {
+      return 'authorized';
+    }
+  }
+  return authenticated ? 'unauthorized' : 'unauthenticated';
+}
+
+export function authorizeUpdateGizmo(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  let authenticated = false;
+  if (context.isAuthenticated('oauth2Auth')) {
+    authenticated = true;
+    if (
+      context.hasScope('oauth2Auth', 'write:gizmos') &&
+      context.hasScope('oauth2Auth', 'admin:gizmos')
+    ) {
+      return 'authorized';
+    }
+  }
+  return authenticated ? 'unauthorized' : 'unauthenticated';
+}
+
+export function authorizeCreateWidget(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  if (context.isAuthenticated('apiKeyAuth')) {
+    return 'authorized';
+  }
+  return 'unauthenticated';
+}
+
+export function authorizeDeleteWidgetFoo(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  if (context.isAuthenticated('apiKeyAuth')) {
+    return 'authorized';
+  }
+  return 'unauthenticated';
+}
+
+export function authorizeGetWidgetFoo(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  if (context.isAuthenticated('apiKeyAuth')) {
+    return 'authorized';
+  }
+  return 'unauthenticated';
+}
+
+export function authorizeGetWidgets(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  if (context.isAuthenticated('apiKeyAuth')) {
+    return 'authorized';
+  }
+  return 'unauthenticated';
+}
+
+export function authorizePutWidget(context?: AuthService): AuthResponse {
+  if (!context) return 'unauthenticated';
+  if (context.isAuthenticated('apiKeyAuth')) {
+    return 'authorized';
+  }
+  return 'unauthenticated';
 }
